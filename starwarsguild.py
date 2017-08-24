b0c8f5168baa74025f3a1ced0016f3d23f01e5fb
vomz <31325041+vomz@users.noreply.github.com>
2017-08-24 18:10:33 -0400

    Add files via upload

--- starwarsguild.py
+++ starwarsguild.py
@@ -9,9 +9,11 @@ start_time = time.time()
 session = requests.session()
 
 #Collect guild members
-x=session.get('https://swgoh.gg/g/602/empire-of-heroes/')
+#x=session.get('https://swgoh.gg/g/602/empire-of-heroes/')
 
-mlist = BeautifulSoup(x.text,'html.parser')
+x=open('C:\\Users\\burkhr\\Desktop\\guildpage.txt','r')
+
+mlist = BeautifulSoup(x.read(),'html.parser')
 
 mlist2=mlist.find_all('td','footable-first-visible')
 
@@ -22,8 +24,8 @@ for i in mlist2:
 for q in members:
     mem = q.split('/')[2]
     
-    # Try accessing a page that requires you to be logged in
     r = session.get('https://swgoh.gg'+q+'collection')
+        
     
     # Translate the web page for use in BeautifulSoup
     collection = BeautifulSoup(r.text,'html.parser')
@@ -40,7 +42,7 @@ for q in members:
     for i in range(0,num_chars):
     	links.append(toons[i].find_all('a','char-portrait-full-link')[0].get('href'))
     
-    # Define needed lists 
+    #Define needed lists 
     toons=[]
     levels=[]
     gear_level=[]
@@ -55,31 +57,57 @@ for q in members:
     
     # Collect information about each of the characters 
     for i in range(0,len(links)):
-    	page=session.get('https://swgoh.gg/'+links[i])
-    	character=BeautifulSoup(page.text,'html.parser')
-    
-    	# name
-    	if 'Chirrut' in character.find_all('a','pc-char-overview-name')[0].text:
-    		toons.append('Chirrut Imwe')
-    	else:
-    		toons.append(character.find_all('a','pc-char-overview-name')[0].text)
-    
-    # Write gear and other information to text files
+        page=session.get('https://swgoh.gg/'+links[i])
+        character=BeautifulSoup(page.read(),'html.parser')
+        
+        # name
+        if 'Chirrut' in character.find_all('a','pc-char-overview-name')[0].text:
+            toons.append('Chirrut Imwe')
+        else:
+            toons.append(character.find_all('a','pc-char-overview-name')[0].text)
+        
+        if len(character.find_all('div','star star7'))>0:
+            stars.append('7')
+        else:
+            if len(character.find_all('div','star star6'))>0:
+                stars.append('6')
+            else:
+                if len(character.find_all('div','star star5'))>0:
+                    stars.append('5')
+                else:
+                    if len(character.find_all('div','star star4'))>0:
+                        stars.append('4')
+                    else:
+                        if len(character.find_all('div','star star3'))>0:
+                            stars.append('3')
+                        else:
+                            if len(character.find_all('div','star star2'))>0:
+                                stars.append('2')
+                            else:
+                                if len(character.find_all('div','star star1'))>0:
+                                    stars.append('1')
+            
+
+
+
+# Write gear and other information to text files
     print 'Writing data.......'
-    
+
     file = open('Data/guildtoons.txt','a+')
     file.write(mem+',')
     for i in range(0,len(toons)):
     	file.write(toons[i]+",")
     file.write('\n')
     file.close()
- 
-#    file = open('Data/guildstars.txt','a+')
-#    file.write(mem+',')
-#    for i in range(0,len(stars)):
-#    	file.write(toons[i]+",")
-#    file.write('\n')
-#    file.close()
+
+
+
+    file = open('C:\\Users\\burkhr\\Desktop\\guildstars.txt','a+')
+    file.write(mem+',')
+    for i in range(0,len(stars)):
+    	file.write(stars[i]+",")
+    file.write('\n')
+    file.close()
 
 # print length the file took to run
 print("--- %s seconds ---" % (time.time() - start_time))
